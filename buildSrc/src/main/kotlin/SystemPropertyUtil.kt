a9b5e10336fd1139db1056db9ba4cfe00a38ee24
Yoonyoul Yoo <yoonyoul@linecorp.com>
2021-01-03 19:28:09 +0900

    fix typo in github workflow

--- buildSrc/src/main/kotlin/SystemPropertyUtil.kt
+++ buildSrc/src/main/kotlin/SystemPropertyUtil.kt
@@ -1,10 +1,7 @@
-import java.lang.Exception
 import java.net.URI
-import java.util.*
 
 fun getProperty(name: String, project: org.gradle.api.Project): String? {
     val systemProperty = System.getenv(name) ?: System.getProperty(name)
-    println("systemProperty: $systemProperty with name: $name")
     return try {
         systemProperty ?: project.property(name)?.toString()
     } catch (e: Exception) {
