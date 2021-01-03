b24b70210f4e11fbe9dc761cfd1e22e29dfa0c04
Yoonyoul Yoo <yoonyoul@linecorp.com>
2021-01-03 17:46:29 +0900

    fix get env variable from github

--- buildSrc/src/main/kotlin/SystemPropertyUtil.kt
+++ buildSrc/src/main/kotlin/SystemPropertyUtil.kt
@@ -1,9 +1,8 @@
 import java.net.URI
 
 fun getProperty(name: String, project: org.gradle.api.Project): String? {
-    val projectProperty = project.property(name)
     val systemProperty = System.getProperty(name) ?: System.getenv(name)
-    return systemProperty ?: projectProperty?.toString()
+    return systemProperty ?: project.property(name)?.toString()
 }
 
 fun mavenReleaseRepoUrl(): URI = URI.create(releasesRepoUrl)
