7d8bc62d6db0071e7d50cd714f7fde4e1f0602bb
Yoonyoul Yoo <yoonyoul@linecorp.com>
2021-01-03 17:56:20 +0900

    fix get env variable from github

--- buildSrc/src/main/kotlin/SystemPropertyUtil.kt
+++ buildSrc/src/main/kotlin/SystemPropertyUtil.kt
@@ -2,6 +2,7 @@ import java.net.URI
 
 fun getProperty(name: String, project: org.gradle.api.Project): String? {
     val systemProperty = System.getenv(name) ?: System.getProperty(name)
+    println("systemProperty: $systemProperty with name: $name")
     return systemProperty ?: project.property(name)?.toString()
 }
 
