b8574421e625dfa580ee530d8dd9f92bc86466f5
yoonyoul <ryukato79@gmail.com>
2021-04-09 16:55:35 +0900

    tests for requestHeaderAppender

--- developers-sdk-kt/src/main/kotlin/com/github/ryukato/link/developers/sdk/api/DefaultRequestHeadersAppendInterceptor.kt
+++ developers-sdk-kt/src/main/kotlin/com/github/ryukato/link/developers/sdk/api/DefaultRequestHeadersAppendInterceptor.kt
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.ryukato.link.developers.sdk.security.NonceGenerator
 import com.github.ryukato.link.developers.sdk.security.SignatureGenerator
+import okhttp3.FormBody
+import okhttp3.Headers
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
 import okhttp3.Response
 import okio.Buffer
 import java.time.Clock
@@ -15,8 +19,9 @@ class DefaultRequestHeadersAppendInterceptor(
     private val nonceGenerator: NonceGenerator,
     private val serviceApiKey: String,
     private val serviceApiSecret: String,
-): Interceptor {
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        @Suppress("DuplicatedCode")
         val timestamp = applicationClock.instant().toEpochMilli()
         val nonce = nonceGenerator.newNonce()
         val request = chain.request()
@@ -29,28 +34,72 @@ class DefaultRequestHeadersAppendInterceptor(
             if (request.body?.contentLength() ?: 0 < 0) {
                 emptyMap()
             } else {
-                val typeObject = object : TypeReference<Map<String, Any>>() {
-                }
-                val buffer = Buffer()
-                request.body?.writeTo(buffer)
-                jacksonObjectMapper().readValue(buffer.inputStream(), typeObject)
+                extractBodyFromRequest(request)
             }
         }
-        val headers = request.headers.newBuilder()
-            .add(SERVICE_API_KEY_HEADER, serviceApiKey)
-            .add(NONCE_HEADER, nonceGenerator.newNonce())
-            .add(SIGNATURE_HEADER, signatureGenerator.generate(
-                serviceApiSecret,
-                request.method,
-                request.url.encodedPath,
-                timestamp,
-                nonce,
-                queryParams,
-                body,
-            ))
-            .add(TIMESTAMP_HEADER, "$timestamp")
-            .build()
+        val signature = signatureGenerator.generate(
+            serviceApiSecret,
+            request.method,
+            request.url.encodedPath,
+            timestamp,
+            nonce,
+            queryParams,
+            body,
+        )
+
+        @Suppress("DuplicatedCode")
+        val headers = newHeaders(request, signature, nonceGenerator.newNonce(), timestamp)
 
         return chain.proceed(request.newBuilder().headers(headers).build())
+    }
+
+    private fun newHeaders(request: Request, signature: String, nonce: String, timestamp: Long): Headers {
+        return request.headers.newBuilder()
+            .add(SERVICE_API_KEY_HEADER, serviceApiKey)
+            .add(NONCE_HEADER, nonce)
+            .add(SIGNATURE_HEADER, signature)
+            .add(TIMESTAMP_HEADER, "$timestamp")
+            .build()
+    }
+
+    private fun extractBodyFromRequest(request: Request): Map<String, Any> = when {
+        request.body?.contentType() == JSON_MEDIA_TYPE -> {
+            jsonBodyToMap(request)
+        }
+        request.body?.contentType() == FORM_MEDIA_TYPE -> {
+            formBodyToMap(request)
+        }
+        else -> {
+            throw IllegalArgumentException("Not supported media type")
+        }
+    }
+
+    private fun jsonBodyToMap(request: Request): Map<String, Any> {
+        return request.body?.let {
+            val typeObject = object : TypeReference<Map<String, Any>>() {}
+            val buffer = Buffer()
+            it.writeTo(buffer)
+            jacksonObjectMapper.readValue(buffer.inputStream(), typeObject)
+        } ?: emptyMap()
+    }
+
+    private fun formBodyToMap(request: Request): Map<String, Any> {
+        return (request.body)?.let {
+            (request.body as FormBody).toMap()
+        } ?: emptyMap()
+    }
+
+    companion object {
+        private val jacksonObjectMapper = jacksonObjectMapper()
+        private val JSON_MEDIA_TYPE = "application/json; charset=UTF-8".toMediaType()
+        private val FORM_MEDIA_TYPE = "application/x-www-form-urlencoded".toMediaType()
+    }
+}
+
+fun FormBody.toMap(): Map<String, String> {
+    return this.let { formBody ->
+        (0 until formBody.size).map {
+            formBody.name(it) to formBody.value(it)
+        }.toMap()
     }
 }
--- developers-sdk-kt/src/test/kotlin/com/github/ryukato/link/developers/sdk/api/DefaultRequestHeadersAppendInterceptorTest.kt
+++ developers-sdk-kt/src/test/kotlin/com/github/ryukato/link/developers/sdk/api/DefaultRequestHeadersAppendInterceptorTest.kt
@@ -1,14 +1,21 @@
 package com.github.ryukato.link.developers.sdk.api
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.ryukato.link.developers.sdk.model.request.OrderBy
 import com.github.ryukato.link.developers.sdk.security.NonceGenerator
 import com.github.ryukato.link.developers.sdk.security.SignatureGenerator
+import okhttp3.FormBody
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -26,7 +33,6 @@ class DefaultRequestHeadersAppendInterceptorTest {
     @BeforeAll
     fun setUpAll() {
         mockWebServer = MockWebServer()
-        mockWebServer.enqueue(MockResponse().setBody("hello, world!"))
         mockWebServer.start(mockServerPort)
     }
 
@@ -37,17 +43,19 @@ class DefaultRequestHeadersAppendInterceptorTest {
 
     @BeforeEach
     fun setUp() {
+        mockWebServer.enqueue(MockResponse().setBody("hello, world!"))
+    }
+
+    @Test
+    fun test_without_body_query_params() {
         interceptor = DefaultRequestHeadersAppendInterceptor(
             clock,
-            signatureGenerator,
+            defaultSignatureGenerator,
             nonceGenerator,
             SERVICE_API_KEY,
             SERVICE_API_SECRET
         )
-    }
 
-    @Test
-    fun test() {
         val loggedHeaders = mutableSetOf<String>()
         val headerCaptureInterceptor = Interceptor { chain ->
             val headers = chain.request().headers
@@ -70,21 +78,256 @@ class DefaultRequestHeadersAppendInterceptorTest {
 
         client.newCall(request).execute()
 
+        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
+    }
+
+    @Test
+    fun test_with_query_params() {
+        val capturedQueryParams = mutableSetOf<String>()
+        val signatureGenerator = object : SignatureGenerator {
+            override fun generate(
+                serviceApiSecret: String,
+                httpMethod: String,
+                path: String,
+                timestamp: Long,
+                nonce: String,
+                pathWithQueryParam: String,
+                body: Map<String, Any?>): String {
+                capturedQueryParams.add(pathWithQueryParam)
+                return "test"
+            }
+
+            override fun generate(
+                serviceApiSecret: String,
+                httpMethod: String,
+                path: String,
+                timestamp: Long,
+                nonce: String,
+                queryParam: Map<String, Any?>,
+                body: Map<String, Any?>): String {
+                capturedQueryParams.add(queryParam.toSortedMap().map { "${it.key}=${it.value}" }.joinToString("&"))
+                return "test"
+            }
+        }
+
+        interceptor = DefaultRequestHeadersAppendInterceptor(
+            clock,
+            signatureGenerator,
+            nonceGenerator,
+            SERVICE_API_KEY,
+            SERVICE_API_SECRET
+        )
+
+        val loggedHeaders = mutableSetOf<String>()
+        val headerCaptureInterceptor = Interceptor { chain ->
+            val headers = chain.request().headers
+            val capturedHeaders = headers.names().map {
+                it
+            }.toSet()
+            loggedHeaders.addAll(capturedHeaders)
+            chain.proceed(chain.request())
+        }
+
+
+        // the order of interceptors is important
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(headerCaptureInterceptor)
+            .build()
+
+        val httpUrl = HttpUrl.Builder()
+            .scheme("http")
+            .host("localhost")
+            .port(mockServerPort)
+            .addQueryParameter("contractId", "test-contract-id")
+            .addQueryParameter("limit", "10")
+            .addQueryParameter("page", "1")
+            .addQueryParameter("orderBy", OrderBy.ASC.name)
+            .build()
+
+        val request: Request = Request.Builder()
+            .url(httpUrl)
+            .build()
+
+        client.newCall(request).execute()
+
+        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
+        assertTrue(capturedQueryParams.any { it.contains("contractId=[test-contract-id]") })
+        assertTrue(capturedQueryParams.any { it.contains("limit=[10]") })
+        assertTrue(capturedQueryParams.any { it.contains("page=[1]") })
+    }
+
+    @Test
+    fun test_with_form_body() {
+        val capturedRequestBody = mutableMapOf<String, Any?>()
+        val signatureGenerator = createCaptureBodySignatureGenerator(capturedRequestBody)
+
+        interceptor = DefaultRequestHeadersAppendInterceptor(
+            clock,
+            signatureGenerator,
+            nonceGenerator,
+            SERVICE_API_KEY,
+            SERVICE_API_SECRET
+        )
+
+        val loggedHeaders = mutableSetOf<String>()
+        val headerCaptureInterceptor = Interceptor { chain ->
+            val headers = chain.request().headers
+            val capturedHeaders = headers.names().map {
+                it
+            }.toSet()
+            loggedHeaders.addAll(capturedHeaders)
+            chain.proceed(chain.request())
+        }
+
+
+        // the order of interceptors is important
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(headerCaptureInterceptor)
+            .build()
+
+        val httpUrl = HttpUrl.Builder()
+            .scheme("http")
+            .host("localhost")
+            .port(mockServerPort)
+            .build()
+        val requestBody = FormBody.Builder()
+            .add("memo", "memo")
+            .add("walletAddress", "walletAddress")
+            .build();
+
+        val request: Request = Request.Builder()
+            .url(httpUrl)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute()
+
+        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
+        assertTrue(capturedRequestBody.containsKey("memo"))
+        assertTrue(capturedRequestBody.containsKey("walletAddress"))
+    }
+
+    @Test
+    fun test_with_json_body() {
+        val capturedRequestBody = mutableMapOf<String, Any?>()
+        val signatureGenerator = createCaptureBodySignatureGenerator(capturedRequestBody)
+
+        interceptor = DefaultRequestHeadersAppendInterceptor(
+            clock,
+            signatureGenerator,
+            nonceGenerator,
+            SERVICE_API_KEY,
+            SERVICE_API_SECRET
+        )
+
+        val loggedHeaders = mutableSetOf<String>()
+        val headerCaptureInterceptor = Interceptor { chain ->
+            val headers = chain.request().headers
+            val capturedHeaders = headers.names().map {
+                it
+            }.toSet()
+            loggedHeaders.addAll(capturedHeaders)
+            chain.proceed(chain.request())
+        }
+
+
+        // the order of interceptors is important
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(headerCaptureInterceptor)
+            .build()
+
+        val httpUrl = HttpUrl.Builder()
+            .scheme("http")
+            .host("localhost")
+            .port(mockServerPort)
+            .build()
+
+        val body = mapOf(
+            "memo" to "memo",
+            "walletAddress" to "walletAddress"
+        )
+
+        val requestBody = jacksonObjectMapper().writeValueAsString(body).toRequestBody("application/json; charset=UTF-8".toMediaType())
+
+        val request: Request = Request.Builder()
+            .url(httpUrl)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute()
+
+        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
+        assertTrue(capturedRequestBody.containsKey("memo"))
+        assertTrue(capturedRequestBody.containsKey("walletAddress"))
+    }
+
+    @Test
+    fun test_with_empty_json_body() {
+        val capturedRequestBody = mutableMapOf<String, Any?>()
+        val signatureGenerator = createCaptureBodySignatureGenerator(capturedRequestBody)
+
+        interceptor = DefaultRequestHeadersAppendInterceptor(
+            clock,
+            signatureGenerator,
+            nonceGenerator,
+            SERVICE_API_KEY,
+            SERVICE_API_SECRET
+        )
+
+        val loggedHeaders = mutableSetOf<String>()
+        val headerCaptureInterceptor = Interceptor { chain ->
+            val headers = chain.request().headers
+            val capturedHeaders = headers.names().map {
+                it
+            }.toSet()
+            loggedHeaders.addAll(capturedHeaders)
+            chain.proceed(chain.request())
+        }
+
+
+        // the order of interceptors is important
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(headerCaptureInterceptor)
+            .build()
+
+        val httpUrl = HttpUrl.Builder()
+            .scheme("http")
+            .host("localhost")
+            .port(mockServerPort)
+            .build()
+
+        val body = emptyMap<String, Any>()
+
+        val requestBody = jacksonObjectMapper().writeValueAsString(body).toRequestBody("application/json; charset=UTF-8".toMediaType())
+
+        val request: Request = Request.Builder()
+            .url(httpUrl)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute()
+
+        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
+        assertTrue(capturedRequestBody.isEmpty())
+    }
+
+
+    companion object {
         val expectedHeaders = setOf(
             "service-api-key",
             "Nonce",
             "Signature",
             "Timestamp"
         )
-        assertEquals(expectedHeaders.toSortedSet(), loggedHeaders.toSortedSet())
-    }
-
-    companion object {
         const val SERVICE_API_KEY = "api-key"
         const val SERVICE_API_SECRET = "secret"
 
         val clock: Clock = Clock.systemUTC()
-        val signatureGenerator = object : SignatureGenerator {
+        val defaultSignatureGenerator = object : SignatureGenerator {
             override fun generate(
                 serviceApiSecret: String,
                 httpMethod: String,
@@ -111,6 +354,35 @@ class DefaultRequestHeadersAppendInterceptorTest {
             override fun newNonce(): String {
                 return "test1234"
             }
+        }
+
+        fun createCaptureBodySignatureGenerator(capturedRequestBody: MutableMap<String, Any?>): SignatureGenerator {
+            return object : SignatureGenerator {
+                override fun generate(
+                    serviceApiSecret: String,
+                    httpMethod: String,
+                    path: String,
+                    timestamp: Long,
+                    nonce: String,
+                    pathWithQueryParam: String,
+                    body: Map<String, Any?>): String {
+                    capturedRequestBody.putAll(body)
+                    return "test"
+                }
+
+                override fun generate(
+                    serviceApiSecret: String,
+                    httpMethod: String,
+                    path: String,
+                    timestamp: Long,
+                    nonce: String,
+                    queryParam: Map<String, Any?>,
+                    body: Map<String, Any?>): String {
+                    capturedRequestBody.putAll(body)
+                    return "test"
+                }
+            }
+
         }
     }
 }
