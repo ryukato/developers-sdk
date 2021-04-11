a9187dc53f841acb77f12130686e43a4e0ff4312
yoonyoul <ryukato79@gmail.com>
2021-04-11 12:56:34 +0900

    tests - signature-gen with request-body

--- developers-sdk-kt/src/test/kotlin/com/github/ryukato/link/developers/sdk/security/SignatureGeneratorTest.kt
+++ developers-sdk-kt/src/test/kotlin/com/github/ryukato/link/developers/sdk/security/SignatureGeneratorTest.kt
@@ -58,7 +58,9 @@ class SignatureGeneratorTest {
         val before = "1617155991000"
 
         `when`(requestBodyFlattener.flatten(emptyMap())).thenAnswer { "" }
-        `when`(queryParameterFlattener.flatten(anyMap())).thenAnswer { "after=$after&before=$before&limit=$limit&msgType=$msgType&orderBy=$orderBy&page=$page" }
+        `when`(queryParameterFlattener.flatten(anyMap())).thenAnswer {
+            "after=$after&before=$before&limit=$limit&msgType=$msgType&orderBy=$orderBy&page=$page"
+        }
         val serviceApiSecret = "7d55f1f5-0f6f-426e-909c-47913aa09e72"
         val httpMethod = "GET"
         val path = "/v1/wallets/tlink1ey2p39e4l78h49pm28z5ms62ycd06sgrprtps5/transactions"
@@ -79,6 +81,41 @@ class SignatureGeneratorTest {
         )
         assertNotNull(signature)
         val expectedSignature = "Iq4lDCgzMmtFrZHuE0b7Xu6PqaqnoVJlG2WxMtuAHWuB8hoG98swyb578LMZMUbHLE3D1ldQA1U4hxSPyxiFSA=="
+        assertEquals(expectedSignature, signature)
+    }
+
+    @Test
+    fun test_with_query_param_n_request_body() {
+        val ownerAddress = "tlink1ey2p39e4l78h49pm28z5ms62ycd06sgrprtps5"
+        val landingUri = "https://my.service.landing/home"
+
+        `when`(requestBodyFlattener.flatten(anyMap())).thenAnswer {
+            "landingUri=$landingUri&ownerAddress=$ownerAddress"
+        }
+        `when`(queryParameterFlattener.flatten(anyMap())).thenAnswer { "requestType=aoa" }
+        val serviceApiSecret = "7d55f1f5-0f6f-426e-909c-47913aa09e72"
+        val httpMethod = "POST"
+        val path = "/v1/users/U9fc03e78e1ae958b1bd3633cfb48acb9/service-tokens/493aba33/request-proxy"
+        val timestamp = 1615593846507
+        val nonce = "fcd9cf1a"
+        val queryParams = mapOf<String, List<String?>>(
+            "requestType" to listOf("aoa")
+        )
+        val requestBody = mapOf(
+            "ownerAddress" to ownerAddress,
+            "landingUri" to landingUri
+        )
+        val signature = signatureGenerator.generate(
+            serviceApiSecret,
+            httpMethod,
+            path,
+            timestamp,
+            nonce,
+            queryParams,
+            requestBody
+        )
+        assertNotNull(signature)
+        val expectedSignature = "hnb+iDG0PPgoByLaUCPtVv5GqcJO1fcKgTO5VolKTITqpRIux7wvCE2d07eY+xXW/553Vq5wLiZ2lX8dZBIOhw=="
         assertEquals(expectedSignature, signature)
     }
 }
